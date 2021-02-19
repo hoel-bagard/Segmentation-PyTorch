ec7926c43e5950360f138ea0151a55740f03b373
Hoel Bagard <hoel.bagard@gmail.com>
2021-02-19 19:45:26 +0900

    Worked on the mutiprocessing part, need to do the prefectching

--- src/dataset/batch_generator.py
+++ src/dataset/batch_generator.py
@@ -1,194 +1,218 @@
 import multiprocessing as mp
 from multiprocessing import shared_memory
-import os
 from typing import (
     Callable,
     Optional,
-    Tuple
+    Final,
+    Any
 )
+from time import time
+from math import ceil
 
 import numpy as np
 
 
 class BatchGenerator:
 
-    def __init__(self, data: np.ndarray, label: np.ndarray, batch_size: int,
-                 num_workers: int,
-                 data_preprocessing_fn: Optional[Callable] = None, label_preprocessing_fn: Optional[Callable] = None,
+    def __init__(self, data: np.ndarray, labels: np.ndarray, batch_size: int,
+                 nb_workers: int = 1,
+                 data_preprocessing_fn: Optional[Callable[[np.ndarray], Any]] = None,
+                 labels_preprocessing_fn: Optional[Callable[[np.ndarray], Any]] = None,
                  shuffle: bool = False):
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.num_workers = num_workers
+        """
+        Args:
+            data: Numpy array with the data. It can be be only path to the datapoints to load (or other forms of data)
+                  if the loading function is given as data_preprocessing_fn.
+            labels: Numpy array with the labels, as for data it can be not yet fully ready labels.
+            nb_workers: Number of workers to use for multiprocessing (>=1).
+            data_preprocessing_fn: If not None, data will be passed through this function.
+            labels_preprocessing_fn: If not None, labels will be passed through this function.
+            shuffle: If True, then dataset is shuffled for each epoch
+        """
 
-        self.data = data
-        self.label = label
+        self.data: Final[np.ndarray] = data
+        self.labels: Final[np.ndarray] = labels
 
-        # TODO: Add possibility to save dataset as hdf5
+        self.batch_size: Final[int] = batch_size
+        self.shuffle: Final[bool] = shuffle
+        self.nb_workers: Final[int] = nb_workers
         self.data_preprocessing_fn = data_preprocessing_fn
-        self.label_preprocessing_fn = label_preprocessing_fn
+        self.labels_preprocessing_fn = labels_preprocessing_fn
 
-        self.dataset_size = len(self.data)
-        self.index_list = np.arange(self.dataset_size)
-        if shuffle:
-            np.random.shuffle(self.index_list)
-
+        # TODOLIST
+        # TODO: Add possibility to save dataset as hdf5
         # TODO: add possibility to drop last batch
-        self.step_per_epoch = (self.dataset_size + (batch_size-1)) // self.batch_size
-        self.last_batch_size = len(self.index_list) % self.batch_size
+        # TODO: Make an Iterator to allow enumerating the dataset
+
+        self.nb_datapoints: Final[int] = len(self.data)
+
+        index_list: np.ndarray = np.arange(self.nb_datapoints)
+        data_batch: np.ndarray = np.asarray([data_preprocessing_fn(entry) if data_preprocessing_fn else entry
+                                             for entry in data[:batch_size]])
+        labels_batch: np.ndarray = np.asarray([labels_preprocessing_fn(entry) if data_preprocessing_fn else entry
+                                               for entry in labels[:batch_size]])
+
+        self.step_per_epoch = (self.nb_datapoints + (batch_size-1)) // self.batch_size
+        self.last_batch_size = self.nb_datapoints % self.batch_size
         if self.last_batch_size == 0:
             self.last_batch_size = self.batch_size
 
         self.epoch = 0
+        self.global_step = 0
         self.step = 0
 
-        first_data = np.array([data_processor(entry) if data_processor else entry
-                               for entry in self.data[self.index_list[:batch_size]]])
-        first_label = np.array([label_processor(entry) if label_processor else entry
-                                for entry in self.label[self.index_list[:batch_size]]])
-        self.batch_data = first_data
-        self.batch_label = first_label
-
-        self.process_id = 'NA'
-        if self.prefetch or self.num_workers > 1:
-            self.cache_memory_indices = shared_memory.SharedMemory(create=True, size=self.index_list.nbytes)
-            self.cache_indices = np.ndarray(
-                self.index_list.shape, dtype=self.index_list.dtype, buffer=self.cache_memory_indices.buf)
-            self.cache_indices[:] = self.index_list
-            self.cache_memory_data = [
-                shared_memory.SharedMemory(create=True, size=first_data.nbytes),
-                shared_memory.SharedMemory(create=True, size=first_data.nbytes)]
-            self.cache_data = [
-                np.ndarray(first_data.shape, dtype=first_data.dtype, buffer=self.cache_memory_data[0].buf),
-                np.ndarray(first_data.shape, dtype=first_data.dtype, buffer=self.cache_memory_data[1].buf)]
-            self.cache_memory_label = [
-                shared_memory.SharedMemory(create=True, size=first_label.nbytes),
-                shared_memory.SharedMemory(create=True, size=first_label.nbytes)]
-            self.cache_label = [
-                np.ndarray(first_label.shape, dtype=first_label.dtype, buffer=self.cache_memory_label[0].buf),
-                np.ndarray(first_label.shape, dtype=first_label.dtype, buffer=self.cache_memory_label[1].buf)]
-        else:
-            self.cache_memory_indices = None
-            self.cache_data = [first_data]
-            self.cache_label = [first_label]
-
-        if self.prefetch:
-            self.prefetch_pipe_parent, self.prefetch_pipe_child = mp.Pipe()
-            self.prefetch_stop = shared_memory.SharedMemory(create=True, size=1)
-            self.prefetch_stop.buf[0] = 0
-            self.prefetch_skip = shared_memory.SharedMemory(create=True, size=1)
-            self.prefetch_skip.buf[0] = 0
-            self.prefetch_process = mp.Process(target=self._prefetch_worker)
-            self.prefetch_process.start()
-            self.num_workers = 0
-        self._init_workers()
+        # Create shared memories for indices, data and labels.
+        self.memories_released = mp.Event()
+        # For data and labels, 2 memories / caches are required for prefetch to work.
+        # (One for the main process to read from, one for the workers to write in)
         self.current_cache = 0
-        self.process_id = 'main'
+        # Indices
+        self.cache_memory_indices = shared_memory.SharedMemory(create=True, size=index_list.nbytes)
+        self.cache_indices = np.ndarray(self.nb_datapoints, dtype=int, buffer=self.cache_memory_indices.buf)
+        self.cache_indices[:] = index_list
+        # Data
+        self.cache_memory_data = [
+            shared_memory.SharedMemory(create=True, size=data_batch.nbytes),
+            shared_memory.SharedMemory(create=True, size=data_batch.nbytes)]
+        self.cache_data = [
+            np.ndarray(data_batch.shape, dtype=data_batch.dtype, buffer=self.cache_memory_data[0].buf),
+            np.ndarray(data_batch.shape, dtype=data_batch.dtype, buffer=self.cache_memory_data[1].buf)]
+        # Labels
+        self.cache_memory_labels = [
+            shared_memory.SharedMemory(create=True, size=labels_batch.nbytes),
+            shared_memory.SharedMemory(create=True, size=labels_batch.nbytes)]
+        self.cache_labels = [
+            np.ndarray(labels_batch.shape, dtype=labels_batch.dtype, buffer=self.cache_memory_labels[0].buf),
+            np.ndarray(labels_batch.shape, dtype=labels_batch.dtype, buffer=self.cache_memory_labels[1].buf)]
+
+        # Create workers
+        self.process_id = "NA"
+        self._init_workers()
+        self.process_id = "main"
 
     def _init_workers(self):
-        if self.num_workers > 1:
-            self.worker_stop = shared_memory.SharedMemory(create=True, size=1)
-            self.worker_stop.buf[0] = 0
-            self.worker_pipes = []
-            self.worker_processes = []
-            for _ in range(self.num_workers):
-                self.worker_pipes.append(mp.Pipe())
-            for worker_index in range(self.num_workers):
-                self.worker_processes.append(mp.Process(target=self._worker, args=(worker_index,)))
-                self.worker_processes[-1].start()
+        """Create workers and pipes / events used to communicate with them"""
+        self.stop_event = mp.Event()
+        self.worker_pipes = [mp.Pipe() for _ in range(self.nb_workers)]
+        self.worker_processes = []
+        for worker_index in range(self.nb_workers):
+            self.worker_processes.append(mp.Process(target=self._worker_fn, args=(worker_index,)))
+            self.worker_processes[-1].start()
+
+    def _worker_fn(self, worker_index: int):
+        self.process_id = f"worker_{worker_index}"
+        pipe = self.worker_pipes[worker_index][1]
+
+        while not self.stop_event.is_set():
+            try:
+                # Check if there is a message to be received. (prevents process from getting stuck)
+                if pipe.poll(0.05):
+                    current_cache, cache_start_index, indices_start_index, nb_elts = pipe.recv()
+                else:
+                    continue
+
+                indices_to_process = self.cache_indices[indices_start_index:indices_start_index+nb_elts]
+
+                # Get the data (and process it)
+                if self.data_preprocessing_fn:  # TODO: one liner with # noqa:E501 ?
+                    processed_data = self.data_preprocessing_fn(self.data[indices_to_process])
+                else:
+                    processed_data = self.data[indices_to_process]
+                # Put the data into the shared memory
+                self.cache_data[current_cache][cache_start_index:cache_start_index+nb_elts] = processed_data
+
+                # Do the same for labels
+                if self.labels_preprocessing_fn:
+                    processed_labels = self.labels_preprocessing_fn(self.labels[indices_to_process])
+                else:
+                    processed_labels = self.labels[indices_to_process]
+                self.cache_labels[current_cache][cache_start_index:cache_start_index+nb_elts] = processed_labels
+
+                # Send signal to the main process to say that everything is ready
+                pipe.send(True)
+            except (KeyboardInterrupt, ValueError):
+                break
+
+    def next_batch(self):
+        self.global_step += 1
+        self.step += 1   # Step starts at 1
+
+        # Check if the current epoch is finished. If it is then start a new one.
+        if (self.step-1) * self.batch_size > self.nb_datapoints:
+            self.epoch += 1
+            self.step = 1
+            np.random.shuffle(self.cache_indices)
+
+        # Prepare arguments for workers and send them
+        current_batch_size = self.batch_size if self.step != self.step_per_epoch else self.last_batch_size
+        self.current_cache = (self.current_cache+1) % 2
+        nb_elts_per_worker = current_batch_size // self.nb_workers
+        for worker_index in range(self.nb_workers):
+            cache_start_index = worker_index * nb_elts_per_worker
+            indices_start_index = (self.step-1) * self.batch_size + cache_start_index
+            nb_elts = nb_elts_per_worker if worker_index != (self.nb_workers-1) else ceil(current_batch_size / self.nb_workers)  # noqa:E501
+            self.worker_pipes[worker_index][0].send((self.current_cache, cache_start_index, indices_start_index, nb_elts))  # noqa:E501
+
+        # Wait for everyworker to have finished preparing its mini-batch
+        for pipe, _ in self.worker_pipes:
+            pipe.recv()
+
+        data_batch = self.cache_data[self.current_cache][:current_batch_size]
+        labels_batch = self.cache_data[self.current_cache][:current_batch_size]
+
+        return data_batch, labels_batch
 
     def __del__(self):
+        self.release()
+
+    def __exit__(self, _exc_type, _exc_value, _traceback):
         self.release()
 
     def __enter__(self):
         return self
 
-    def __exit__(self, _exc_type, _exc_value, _traceback):
-        self.release()
-
     def release(self):
-        """Terminates all subprocesses and releases ressources"""
-        pass
+        """Terminates all workers and releases all the shared ressources"""
+        # Closes acces to the shared memories
+        for shared_mem in self.cache_memory_data + self.cache_memory_labels + [self.cache_memory_indices]:
+            shared_mem.close()
 
-    def _worker(self, worker_index: int):
-        self.process_id = f'worker_{worker_index}'
-        self.num_workers = 0
-        self.current_cache = 0
-        parent_cache_data = self.cache_data
-        parent_cache_label = self.cache_label
-        cache_data = np.ndarray(self.cache_data[0].shape, dtype=self.cache_data[0].dtype)
-        cache_label = np.ndarray(self.cache_label[0].shape, dtype=self.cache_label[0].dtype)
-        self.cache_data = [cache_data]
-        self.cache_label = [cache_label]
-        self.index_list[:] = self.cache_indices
-        pipe = self.worker_pipes[worker_index][1]
+        if self.process_id == "main":
+            self.stop_event.set()   # Sends signal to stop to all the workers
 
-        while self.worker_stop.buf is not None and self.worker_stop.buf[0] == 0:
-            try:
-                current_cache, batch_index, start_index, self.batch_size = pipe.recv()
-                if self.batch_size == 0:
-                    continue
-                self.index_list = self.cache_indices[start_index:start_index + self.batch_size].copy()
+            # Terminates all the workers
+            end_time = time() + 5  # Maximum waiting time (for all processes)
+            for pipe, worker in zip(self.worker_pipes, self.worker_processes):
+                pipe[0].close()
+                worker.join(timeout=max(0.0, end_time - time()))
+                self.worker_pipes.remove(pipe)
+                self.worker_processes.remove(worker)
 
-                self.cache_data[0] = cache_data[:self.batch_size]
-                self.cache_label[0] = cache_label[:self.batch_size]
-                self._next_batch()
-                parent_cache_data[current_cache][batch_index:batch_index + self.batch_size] = self.cache_data[
-                    self.current_cache][:self.batch_size]
-                parent_cache_label[current_cache][batch_index:batch_index + self.batch_size] = self.cache_label[
-                    self.current_cache][:self.batch_size]
-                pipe.send(True)
-            except KeyboardInterrupt:
-                break
-            except ValueError:
-                break
-
-    def _worker_next_batch(self):
-        index_list = self.index_list[self.step * self.batch_size:(self.step + 1) * self.batch_size]
-        batch_len = len(index_list)
-        indices_per_worker = batch_len // self.num_workers
-        if indices_per_worker == 0:
-            indices_per_worker = 1
-
-        worker_params = []
-        batch_index = 0
-        start_index = self.step * self.batch_size
-        for _worker_index in range(self.num_workers - 1):
-            worker_params.append([self.current_cache, batch_index, start_index, indices_per_worker])
-            batch_index += indices_per_worker
-            start_index += indices_per_worker
-        worker_params.append([
-            self.current_cache, batch_index, start_index,
-            batch_len - ((self.num_workers - 1) * indices_per_worker)])
-
-        if indices_per_worker == 1 and batch_len < self.num_workers:
-            worker_params = worker_params[:batch_len]
-
-        for params, (pipe, _) in zip(worker_params, self.worker_pipes):
-            pipe.send(params)
-        for _, (pipe, _) in zip(worker_params, self.worker_pipes):
-            pipe.recv()
-
-    def next_bath(self):
-        pass
+            if not self.memories_released.is_set():
+                # Requests for all the shared memories to be destroyed
+                print("Releasing shared memories")
+                for shared_mem in self.cache_memory_data + self.cache_memory_labels + [self.cache_memory_indices]:
+                    shared_mem.unlink()
+                self.memories_released.set()
 
 
 if __name__ == '__main__':
+    # TODO: Make the function into an actual test with asserts
     def test():
         data = np.array(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], dtype=np.uint8)
         label = np.array(
             [.1, .2, .3, .4, .5, .6, .7, .8, .9, .10, .11, .12, .13, .14, .15, .16, .17, .18], dtype=np.uint8)
 
-        for data_processor in [None, lambda x:x]:
-            for prefetch in [True, False]:
-                for num_workers in [3, 1]:
-                    print(f'{data_processor=} {prefetch=} {num_workers=}')
-                    with BatchGenerator(data, label, 5, data_processor=data_processor,
-                                        prefetch=prefetch, num_workers=num_workers) as batch_generator:
-                        for _ in range(19):
-                            print(batch_generator.batch_data, batch_generator.epoch, batch_generator.step)
-                            batch_generator.next_batch()
-                        raise KeyboardInterrupt
-                    print()
+        data_preprocessing_fn = None
+
+        for nb_workers in [2]:
+            print(f'{nb_workers=}')
+            with BatchGenerator(data, label, 5, data_preprocessing_fn=data_preprocessing_fn,
+                                nb_workers=nb_workers) as batch_generator:
+                for _ in range(19):
+                    data_batch, labels_batch = batch_generator.next_batch()
+                    # print(f"{batch_generator.epoch=}, {batch_generator.step=}")
+                    print(f"{data_batch=}, {labels_batch=}")
 
     test()
