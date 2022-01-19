3844979bbeb1cf7c0ee1dfd74c2da20c87aa1bb1
Hoel Bagard <hoel.bagard@gmail.com>
2022-01-19 15:26:56 +0900

    Fixed output path issue.

--- utils/cut_images.py
+++ utils/cut_images.py
@@ -9,8 +9,8 @@ def main():
     parser = argparse.ArgumentParser("Cuts images and corresponding masks into small tiles")
     parser.add_argument("data_path", type=Path, help="Path to the dataset")
     parser.add_argument("output_path", type=Path, help="Output path")
-    parser.add_argument("--tile_size", "--ts", nargs=2, default=[256, 256], type=int, help="Size of the tiles (w, h)")
-    parser.add_argument("--stride", "--s", nargs=2, default=[100, 100], type=int, help="Strides (w, h)")
+    parser.add_argument("--tile_size", "-ts", nargs=2, default=[256, 256], type=int, help="Size of the tiles (w, h)")
+    parser.add_argument("--stride", "-s", nargs=2, default=[100, 100], type=int, help="Strides (w, h)")
     args = parser.parse_args()
 
     data_path: Path = args.data_path
@@ -21,20 +21,20 @@ def main():
     stride_width, stride_height = args.stride
 
     exts = [".jpg", ".png"]
-    file_list = list([p for p in data_path.rglob('*') if p.suffix in exts and "mask" not in str(p)])
-    nb_imgs = len(file_list)
-    for i, file_path in enumerate(file_list):
-        msg = f"Processing image {file_path.name} ({i+1}/{nb_imgs})"
+    img_path_list = list([p for p in data_path.rglob('*') if p.suffix in exts and "mask" not in str(p)])
+    nb_imgs = len(img_path_list)
+    for i, img_path in enumerate(img_path_list):
+        msg = f"Processing image {img_path.name} ({i+1}/{nb_imgs})"
         print(msg + ' ' * (get_terminal_size(fallback=(156, 38)).columns - len(msg)), end='\r')
 
-        img_mask_path = Path(str(file_path.with_suffix('')) + "_mask.png")
-        assert img_mask_path.exists(), f"\nMask for image {file_path} is missing"
+        img_mask_path = Path(str(img_path.with_suffix('')) + "_mask.png")
+        assert img_mask_path.exists(), f"\nMask for image {img_path} is missing"
 
-        img = cv2.imread(str(file_path))
+        img = cv2.imread(str(img_path))
         height, width, _ = img.shape
         img_mask = cv2.imread(str(img_mask_path))
         assert img_mask.shape[0] == height and img_mask.shape[1] == width, (
-            f"\nShape of the image and the mask do not match for image {file_path}")
+            f"\nShape of the image and the mask do not match for image {img_path}")
 
         tile_index = 0
         for x in range(0, width-tile_width, stride_width):
@@ -42,12 +42,15 @@ def main():
                 tile = img[y:y+tile_height, x:x+tile_width]
                 tile_mask = img_mask[y:y+tile_height, x:x+tile_width]
 
-                new_tile_name = file_path.stem + '_' + str(tile_index).zfill(5) + file_path.suffix
-                tile_path = output_path / new_tile_name
+                rel_path = img_path.relative_to(data_path)
+                new_tile_name = img_path.stem + '_' + str(tile_index).zfill(5) + img_path.suffix
+                tile_path = output_path / rel_path.parent / new_tile_name
+                tile_path.parent.mkdir(exist_ok=True, parents=True)
                 cv2.imwrite(str(tile_path), tile)
 
-                new_tile_mask_name = img_mask_path.stem + '_' + str(tile_index).zfill(5) + img_mask_path.suffix
-                tile_mask_path = output_path / new_tile_mask_name
+                new_tile_mask_name = (''.join(img_mask_path.stem.split('_')[:-1])
+                                      + '_' + str(tile_index).zfill(5) + "_mask.png")
+                tile_mask_path = output_path / rel_path.parent / new_tile_mask_name
                 cv2.imwrite(str(tile_mask_path), tile_mask)
 
                 tile_index += 1
