5e5362d81d85696d6d6c0947f6f236c23e6805b5
Hoel Bagard <hoel.bagard@gmail.com>
2022-02-25 22:04:59 +0900

    Added padding between masks.

--- src/utils/draw_seg.py
+++ src/utils/draw_seg.py
@@ -29,7 +29,9 @@ def draw_segmentation_danger_p(imgs: npt.NDArray[np.uint8],
         np.ndarray: RGB segmentation masks and original image (in one image)
     """
     height, width, _ = imgs[0].shape
-    mask_width, mask_height = width // 2, height // 2
+    padding_masks = p = 5  # Padding//2 in pixels between the masks.
+    mask_width, mask_height = width // 2 - padding_masks, height // 2 - padding_masks
+    white = (255, 255, 255)
 
     put_text = partial(cv2.putText, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 0, 0,),
                        thickness=1, lineType=cv2.LINE_AA)
@@ -43,37 +45,49 @@ def draw_segmentation_danger_p(imgs: npt.NDArray[np.uint8],
 
     out_imgs = []
     for img, pred_mask, label_mask in zip(imgs, masks_preds, masks_labels):
+        # 0. Unpack the masks
         pred_cls_mask, pred_danger_mask = pred_mask[..., 0], pred_mask[..., 1]
         label_cls_mask, label_danger_mask = label_mask[..., 0], label_mask[..., 1]
 
-        # Make the classification masks into RGB image for visualization.
+        # 1. Transform the masks into easily interpreted images.
+        # 1.1 Make the classification masks into RGB images for visualization.
         pred_cls_mask = np.asarray(idx_to_color[pred_cls_mask], dtype=np.uint8)
         label_cls_mask = np.asarray(idx_to_color[label_cls_mask], dtype=np.uint8)
 
-        # For the danger level, just go from dark for safe to white for danger (high danger lvl)
+        # 1.2 For the danger level, just go from dark for safe to white for danger (high danger lvl)
         pred_danger_mask = (255 * pred_danger_mask / max_danger_lvl).astype(np.uint8)
         pred_danger_mask = cv2.cvtColor(pred_danger_mask, cv2.COLOR_GRAY2RGB)
         label_danger_mask = (255 * label_danger_mask / max_danger_lvl).astype(np.uint8)
         label_danger_mask = cv2.cvtColor(label_danger_mask, cv2.COLOR_GRAY2RGB)
 
-        # Scale all the masks to fit them on the final image.
-        # The Labels' width/height are the way they are to handle the (unlikely) case where the images have an odd size
+        # 2. Scale all the masks to fit them on the final image.
+        # 2.1 Resize te masks.
+        # The modulo in the labels' resize is to handle the (unlikely) case where the images have an odd size
         pred_cls_mask = cv2.resize(pred_cls_mask, (mask_width, mask_height), interpolation=cv2.INTER_NEAREST_EXACT)
-        label_cls_mask = cv2.resize(label_cls_mask, (width-mask_width, height-mask_height),
+        label_cls_mask = cv2.resize(label_cls_mask, (mask_width + width % 2, mask_height + height % 2),
                                     interpolation=cv2.INTER_NEAREST_EXACT)
         pred_danger_mask = cv2.resize(pred_danger_mask, (mask_width, mask_height),
                                       interpolation=cv2.INTER_NEAREST_EXACT)
-        label_danger_mask = cv2.resize(label_danger_mask, (width-mask_width, height-mask_height),
+        label_danger_mask = cv2.resize(label_danger_mask, (mask_width + width % 2, mask_height + height % 2),
                                        interpolation=cv2.INTER_NEAREST_EXACT)
 
-        # Combine all the masks into one image:
+        # 2.2 Add padding between the masks (otherwise it's hard to see where they begin/end)
+        pred_cls_mask = cv2.copyMakeBorder(pred_cls_mask, 0, p, 0, p, cv2.BORDER_CONSTANT, value=white)
+        label_cls_mask = cv2.copyMakeBorder(label_cls_mask, 0, p, p, 0, cv2.BORDER_CONSTANT, value=white)
+        pred_danger_mask = cv2.copyMakeBorder(pred_danger_mask, p, 0, 0, p, cv2.BORDER_CONSTANT, value=white)
+        label_danger_mask = cv2.copyMakeBorder(label_danger_mask, p, 0, p, 0, cv2.BORDER_CONSTANT, value=white)
+
+        # 3. Combine everything into one image
+        # 3.1 Combine all the masks.
         cls_mask = cv2.hconcat((pred_cls_mask, label_cls_mask))
         danger_mask = cv2.hconcat((pred_danger_mask, label_danger_mask))
         masks = cv2.vconcat((cls_mask, danger_mask))
 
+        # 3.2 Split the image and the disparity map
         grayscale_img = cv2.cvtColor(img[..., 0], cv2.COLOR_GRAY2RGB)  # The "classic" input image
         disp = cv2.cvtColor(img[..., 1], cv2.COLOR_GRAY2RGB)  # The disparity map
 
+        # 3.3 Concat everything
         out_img_top = cv2.hconcat((grayscale_img, disp))
         out_img_bot = cv2.hconcat((masks, text_img))
         out_img = cv2.vconcat((out_img_top, out_img_bot))
